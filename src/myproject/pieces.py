a9019480f3e29caa5c6d8d19e32ff8df3b092b87
Danté G <16039915+DarkMasquerader@users.noreply.github.com>
2026-06-07 01:57:34 +0100

    refactor: merge move.py into pieces.py

--- src/myproject/pieces.py
+++ src/myproject/pieces.py
@@ -2,12 +2,59 @@ from __future__ import annotations
 
 from abc import abstractmethod
 from typing import Tuple, TYPE_CHECKING
-from .move import ChessMove, HORIZONTAL_MOVES, DIAGONAL_MOVES
-from ..utils.helper import *
+from .utils.helper import *
 import copy
 
 if TYPE_CHECKING:
     from .player import PlayerType
+
+
+class ChessMove:
+
+    def __init__(
+        self,
+        x_axis: int,
+        y_axis: int,
+        *,
+        must_take: bool = False,
+        req_no_check: bool = False,
+        first_move_only: bool = False,
+        no_take: bool = False,
+    ):
+        self.x_axis = x_axis
+        self.y_axis = y_axis
+        self.must_take = must_take
+        self.req_no_check = req_no_check
+        self.first_move_only = first_move_only
+        self.no_take = no_take
+
+    def invert_moveset(self) -> None:
+        self.x_axis = -self.x_axis
+        self.y_axis = -self.y_axis
+
+    def get_loc(self) -> Tuple[str, str]:
+        return (LETTER_LOC[self.x_axis], str(self.y_axis))
+
+    def get_ords(self) -> Tuple[int, int]:
+        return (self.x_axis, self.y_axis)
+
+
+HORIZONTAL_MOVES = [
+    *[ChessMove(x, y) for x, y in zip([0] * 7, range(1, 7))],  # Up
+    *[ChessMove(x, y) for x, y in zip([0] * 7, range(-1, -7, -1))],  # Down
+    *[ChessMove(x, y) for x, y in zip(range(1, 7), [0] * 7)],  # Right
+    *[ChessMove(x, y) for x, y in zip(range(-1, -7, -1), [0] * 7)],  # Left
+]
+
+
+DIAGONAL_MOVES = [
+    *[ChessMove(x, y) for x, y in zip(range(1, 7), range(1, 7))],  # Up right
+    *[ChessMove(x, y) for x, y in zip(range(-1, -7, -1), range(1, 7))],  # Up left
+    *[ChessMove(x, y) for x, y in zip(range(1, 7), range(-1, -7, -1))],  # Down right
+    *[
+        ChessMove(x, y) for x, y in zip(range(-1, -7, -1), range(-1, -7, -1))
+    ],  # Down left
+]
 
 
 class ChessPiece:
